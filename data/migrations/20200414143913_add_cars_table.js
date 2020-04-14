78478c9ece09dd38793c2fdc53e3cb37706d1ad2
M Groesbeck <m.groesbeck@gmail.com>
2020-04-14 15:39:29 -0700

    C/R endpoints built.

--- data/migrations/20200414143913_add_cars_table.js
+++ data/migrations/20200414143913_add_cars_table.js
@@ -2,6 +2,7 @@
 exports.up = function(knex) {
   return knex.schema.createTable('cars', tbl => {
       tbl.increments('id');
+      tbl.string('vin').notNullable();
       tbl.string('make').notNullable();
       tbl.string('model').notNullable();
       tbl.decimal('mileage').notNullable();
--- data/routers/car-router.js
+++ data/routers/car-router.js
@@ -1,20 +1,54 @@
-const express = require('express');
-const knex = require('knex');
+const express = require("express");
+const knex = require("knex");
 
-const knexfile = require('../../knexfile.js');
+const knexfile = require("../../knexfile.js");
 
 const db = knex(knexfile.development);
 
 const router = express.Router();
 
-router.get('', (req, res) => {
-    db('cars')
-    .then(cars => {
-        res.json(cars);
+router.get("/", (req, res) => {
+  db("cars")
+    .then((cars) => {
+      res.json(cars);
     })
-    .catch(err => {
-        res.status(500).json({ message: "Failed to retrieve car data." });
-    })
+    .catch((err) => {
+      res.status(500).json({ message: "Failed to retrieve car data." });
+    });
+});
+
+router.get("/:id/", (req, res) => {
+  if (req.params.id) {
+    db("cars")
+      .where({ id: req.params.id })
+      .first()
+      .then((car) => {
+        res.json(car);
+      })
+      .catch((err) => {
+        res.status(500).json({ message: "Failed to retrieve car" });
+      });
+  } else {
+    res.status(400).json({ message: "Car ID required." });
+  }
+});
+
+router.post("/", (req, res) => {
+    if(req.body.vin && req.body.make && req.body.model && req.body.mileage){
+        db('cars').insert(req.body)
+        .then(ids => {
+            db('cars').where({ id: ids[0] })
+            .then(newCar => {
+                res.status(201).json(newCar);
+            })
+        })
+        .catch (err => {
+            console.log('POST error', err);
+            res.status(500).json({ message: "Failed to store data" });
+          });
+    } else {
+        res.status(400).json({ message: "Requires VIN, make, model, and mileage." });
+    }
 })
 
 module.exports = router;
